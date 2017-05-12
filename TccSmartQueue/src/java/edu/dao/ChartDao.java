790f1a6cf3344cc69e313cefc19c6568d2c27f9e
brunamarcolino <bruna_marcolino@hotmail.com>
2017-05-12 09:12:08 -0300

    produtividade

--- TccSmartQueue/src/java/edu/dao/ChartDao.java
+++ TccSmartQueue/src/java/edu/dao/ChartDao.java
@@ -61,25 +61,36 @@ public class ChartDao extends Dao{
         }
     }
     
-    public Chart relatorio_produtividade(String parametro_data_ini, String parametro_data_fim, String parametro_atendente) {
+    public List<Chart> relatorio_produtividade(String parametro_data_ini, String parametro_data_fim, String parametro_atendente) {
         Connection conn = null;
-        
+
         try {
             conn = getConnection();
-
-            String sql = "SELECT  u.nm_usuario, s.data_senha, COUNT(1) qtde_atendimento FROM tab_senhas S inner join tab_usuarios u on u.id_usuario = s.id_usuario WHERE s.data_senha between STR_TO_DATE("+parametro_data_ini+", '%Y-%m-%d') and STR_TO_DATE("+parametro_data_fim+", '%Y-%m-%d') and ("+parametro_atendente+"= 'TODOS' or s.id_usuario = "+parametro_atendente+")";
+            List<Chart> charts = new ArrayList<Chart>();
             
-            System.out.println(sql);
-            
+            //String sql = "SELECT u.nm_usuario , s.data_senha, ifnull(TIME_FORMAT(AVG(TIMEDIFF(s.data_atendimento_fim,s.data_atendimento_ini)),'%T'),0) tm FROM tab_senhas s inner join tab_usuarios u on u.id_usuario = s.id_usuario WHERE s.data_atendimento_fim is not null group by u.nm_usuario, s.data_senha";
+            String sql = "SELECT u.nm_usuario, IFNULL(count(1),0) previsao FROM tab_senhas s inner join tab_usuarios u on u.id_usuario = s.id_usuario WHERE  s.data_senha between STR_TO_DATE(?, '%Y-%m-%d') and STR_TO_DATE(?, '%Y-%m-%d') and (? = 'TODOS' or s.id_usuario = ?) and s.data_atendimento_fim is not null group by u.nm_usuario";  
             PreparedStatement stmt = conn.prepareStatement(sql);
-            //stmt.setString(1, nm_usuario);
-            //stmt.setString(2, senha_usuario);
+            stmt.setString(1, parametro_data_ini);
+            stmt.setString(2, parametro_data_fim);
+            stmt.setString(3, parametro_atendente);
+            stmt.setString(4, parametro_atendente);
+
             ResultSet result = stmt.executeQuery();
             
-            Chart chart = new Chart();
             
-            
-            return chart;
+            while (result.next()){
+               Chart chart = new Chart(); 
+               
+               chart.setValor_x(result.getString("previsao"));
+               chart.setValor_y(result.getString("nm_usuario"));
+               //chart.setValor_tooltip(result.getString("tm"));
+               System.out.println("Retorno: " + chart.getValor_x() + chart.getValor_tooltip() + chart.getValor_y());
+               
+               charts.add(chart);
+            }
+            System.out.println("DAO " + charts);
+            return charts;
         } catch (Exception ex) {
             ex.printStackTrace();
             return null;
